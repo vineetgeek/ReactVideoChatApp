843bbf19d250b573fce01c382bb38b24e55e7a91
vineet srivastav <vineet9798@gmail.com>
2021-07-04 00:18:14 +0530

    make it better

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ io.on("connection", (socket) => {
 	})
 })
 
-server.listen(5000, () => console.log("server is running on port 5000"))
+server.listen(process.env.PORT || 5000, () => console.log("server is running on port 5000"))
