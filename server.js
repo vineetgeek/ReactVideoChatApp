28f70ee0baae8c8175d3d7bc6a351f4f9da3e5fa
vineet srivastav <vineet9798@gmail.com>
2021-07-04 00:12:38 +0530

    make it better

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 const express = require("express")
 const http = require("http")
-const app = express()
 const server = http.createServer(app)
-const io = require("socket.io")(server, {
-	cors: {
-		origin: "https://vineetgeekvc.netlify.app/",
-		methods: [ "GET", "POST" ]
-	}
-})
+const io = require("socket.io")(server)
 
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id)
